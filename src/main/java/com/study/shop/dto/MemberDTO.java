f3d84b64ab61e72145955e60bfb739b343f5aa1d
leegyuhwan <rbghks4677@naver.com>
2024-04-03 20:33:12 +0900

    24.04.03 자취

---
+++ src/main/java/com/study/shop/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.study.shop.dto;
+
+
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+public class MemberDTO {
+    private Long id;
+    private String username;
+    private String displayName;
+    private String password;
+}
